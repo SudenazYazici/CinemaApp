466b95a1b8b02700997054cc2752fc15f815d2ed
SudenazYazici <sudenyazici@gmail.com>
2024-07-15 14:17:12 +0300

    SeatDto fixed

--- CinemaApp/Dto/SeatDto.cs
+++ CinemaApp/Dto/SeatDto.cs
@@ -7,7 +7,7 @@ namespace CinemaApp.Dto
         public int Id { get; set; }
         public int SeatNum { get; set; }
         public int CinemaHallId { get; set; }
-        public CinemaHall CinemaHall { get; set; }
-        public Ticket Ticket { get; set; }
+        //public CinemaHall CinemaHall { get; set; }
+        //public Ticket Ticket { get; set; }
     }
 }
